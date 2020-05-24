4d5a302cdbee8abb276c8551b3546e49396b6546
Franciole <franciole.bezerra@gmail.com>
2020-05-24 01:10:57 -0300

    Projecao dos dados do autor com DTO

---
+++ src/main/java/com/curso/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,38 @@
+package com.curso.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.curso.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public String id;
+	public String name;
+
+	public AuthorDTO() {
+	}
+
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
